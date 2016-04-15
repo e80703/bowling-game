9bd1eec5d6b4d7e734e1762a478f46ed15d7541d
chihyuwang <chihyuwang@eztravel.com.tw>
2016-04-15 15:03:35 +0800

    Initial commit

---
+++ src/test/java/com/bowling/service/BowlingGameTest.java
@@ -0,0 +1,12 @@
+package com.bowling.service;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class BowlingGameTest extends TestCase {
+  @Test
+  public void testGame(){
+    
+  }
+}
