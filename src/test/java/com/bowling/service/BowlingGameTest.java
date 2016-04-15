9dff12e117116f16a039259a16d0e985dbd7ccc5
chihyuwang <chihyuwang@eztravel.com.tw>
2016-04-15 16:15:26 +0800

    add bowling game api and test

--- src/test/java/com/bowling/service/BowlingGameTest.java
+++ src/test/java/com/bowling/service/BowlingGameTest.java
@@ -1,12 +1,56 @@
 package com.bowling.service;
 
-import org.junit.Test;
-
 import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase {
-  @Test
-  public void testGame(){
-    
+  
+  private Game game;
+  
+  protected void setUp() {
+    game = new Game();
+  }
+  
+  private void rollMany(int n, int pins){
+    for (int i = 0; i < n; i++) {
+      game.roll(pins);
+    }
+  }
+  
+  public void testGame() throws Exception {
+    rollMany(20, 0);
+    assertEquals(0, game.score());
+  }
+  
+  public void testAllOnes() throws Exception {
+    rollMany(20, 1);
+    assertEquals(20, game.score());
+  }
+  
+  public void testOneSpare() throws Exception {
+    rollSpare();
+    game.roll(3);
+    rollMany(17, 0);
+    assertEquals(16, game.score());
+  }
+  
+  public void testOneStrike() throws Exception {
+    rollStrike();
+    game.roll(4);
+    game.roll(3);
+    assertEquals(24, game.score());
+  }
+  
+  public void testPerfectGame() throws Exception {
+    rollMany(12, 10);
+    assertEquals(300, game.score());
+  }
+  
+  private void rollSpare(){
+    game.roll(5);
+    game.roll(5);
+  }
+  
+  private void rollStrike(){
+    game.roll(10);    
   }
 }
